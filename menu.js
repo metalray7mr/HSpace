5b6cfc9932c76e33af112b78a1057ccf24b3987e
abhishek <abhishek@abhisheks-MacBook-Air.local>
2020-05-12 21:55:47 +0530

    add meta data

--- menu.js
+++ menu.js
@@ -3,9 +3,12 @@ class menu extends object{
         super();
         this.hight=100;
         this.width=100;
+        
     }
 
     draw(){
+        noStroke()
+        fill(123,63,81)
         circle(700,20,30)
 
       
